28f6854591b75891c2d182367f13b0c6ef09463e
Bharathwaj-D <113932736+Bharathwaj-D@users.noreply.github.com>
2023-09-26 17:04:26 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ data "aws_ami" "web_ami"{
     values = ["amzn2-ami-hvm*"]
   }
 }
-resource "aws_instance" "web" {
+resource "aws_instance" "web1" {
   ami           =  data.aws_ami.web_ami.id 
   instance_type =  var.instance_type 
 }
