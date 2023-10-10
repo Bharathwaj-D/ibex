5a7d5f3b21b4898b54a74126445695a7e3d52b00
Bharathwaj-D <113932736+Bharathwaj-D@users.noreply.github.com>
2023-10-10 14:04:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -10,7 +10,6 @@ data "aws_ami" "web_ami"{
 resource "aws_instance" "web" {
     ami           =  data.aws_ami.web_ami.id 
     instance_type =  var.instance_type 
-    subnet_id = var.public_subnet
     tags = {
            Name = var.enviroment_name
     }
